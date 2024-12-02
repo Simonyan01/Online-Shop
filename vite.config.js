8476f11dba93b667d72be16a70d02310504218e9
Simonyan01 <lyov.simonyan.01@gmail.com>
2024-12-02 20:05:36 +0400

    added aliases

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
@@ -11,6 +11,6 @@ export default defineConfig({
       '@components': '/src/components',
       '@data': '/src/data',
       '@utils': '/src/utils',
-    },
-  },
+    }
+  }
 })
